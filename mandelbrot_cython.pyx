1598b5ac44d674e5d04224f39c9bfd6984ae5a30
RudyRM <134332636+RudyRM@users.noreply.github.com>
2026-07-02 04:07:36 -0400

    Update mandelbrot_cython.pyx

--- mandelbrot_cython.pyx
+++ mandelbrot_cython.pyx
@@ -3,7 +3,7 @@ import numpy as np
 cimport numpy as cnp
 from cython.parallel import prange
 
-# 1. Versión Cython pura (Compilación estática - Clase 4)
+#Cython
 def compute_cython(double[:, :] grid, int max_iter):
     cdef int i, j, k
     cdef int height = grid.shape[0]
@@ -29,16 +29,14 @@ def compute_cython(double[:, :] grid, int max_iter):
             output[i, j] = k
     return np.asarray(output)
 
-# 2. Versión Cython + OpenMP (CPU Multi-thread sin GIL - Clase 6)
+#Cython + OpenMP
 def compute_openmp(double[:, :] grid, int max_iter, int num_threads):
-    # Todas las declaraciones cdef se quedan arriba cumpliendo la regla de Cython
     cdef int i, j, k
     cdef int height = grid.shape[0]
     cdef int width = grid.shape[1]
     cdef double c_re, c_im, z_re, z_im, z_re_sq, z_im_sq
     cdef int[:, :] output = np.zeros((height, width), dtype=np.int32)
     
-    # prange paraleliza el ciclo externo distribuyendo las filas en hilos nativos
     for i in prange(height, nogil=True, num_threads=num_threads, schedule='dynamic'):
         for j in range(width):
             c_re = -2.0 + (j * 3.0 / width)
@@ -46,8 +44,7 @@ def compute_openmp(double[:, :] grid, int max_iter, int num_threads):
             z_re = 0.0
             z_im = 0.0
             
-            # Usamos un for estático en rango. Cython y OpenMP interpretan automáticamente 
-            # la variable 'k' del ciclo como PRIVADA para cada hilo, eliminando el error de reducción.
+        
             for k in range(max_iter):
                 z_re_sq = z_re * z_re
                 z_im_sq = z_im * z_im
